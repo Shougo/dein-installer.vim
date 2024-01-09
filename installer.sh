11e9af56fe00edb06d19ee8e7f8bfb222230699b
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-01-09 09:15:07 +0900

    Fix installer

--- installer.sh
+++ installer.sh
@@ -126,14 +126,10 @@ call dein#end()
 " Attempt to determine the type of a file based on its name and possibly its
 " contents. Use this to allow intelligent auto-indenting for each filetype,
 " and for plugins that are filetype specific.
-if has('filetype')
-  filetype indent plugin on
-endif
+filetype indent plugin on
 
 " Enable syntax highlighting
-if has('syntax')
-  syntax on
-endif
+syntax enable
 
 " Uncomment if you want to install not-installed plugins on startup.
 "if dein#check_install()
